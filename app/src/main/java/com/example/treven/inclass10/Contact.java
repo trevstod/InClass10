3a43970bff0e47a3a8eec419eb2dd4e9c40b02c7
Corivana <corinna.wolfe@gmail.com>
2018-04-09 19:14:50 -0400

    Contact List, Contact Adapter, Contacts Activity
    Recycler View Started in Contacts Activity.

---
+++ app/src/main/java/com/example/treven/inclass10/Contact.java
@@ -0,0 +1,48 @@
+package com.example.treven.inclass10;
+
+<<<<<<< HEAD
+import java.util.ArrayList;
+
+public class Contact{
+
+    String fName, lName, eMail, phone;
+
+    public Contact(String fName, String lName, String eMail, String phone) {
+        this.fName = fName;
+        this.lName = lName;
+        this.eMail = eMail;
+        this.phone = phone;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- app/src/main/java/com/example/treven/inclass10/MainActivity.java
+++ app/src/main/java/com/example/treven/inclass10/MainActivity.java
@@ -1,13 +1,100 @@
 package com.example.treven.inclass10;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText emailField;
+    EditText passField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailField = findViewById(R.id.loginEmailField);
+        passField = findViewById(R.id.loginPasswordField);
+        //passField.setTransformationMethod(new AsteriskPasswordTransformationMethod());
+
+
+        SharedPreferences pref = getSharedPreferences("info", MODE_PRIVATE);
+
+
+
+        findViewById(R.id.signupButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupIntent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(signupIntent);
+            }
+        });
+
+
+
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.signInWithEmailAndPassword(emailField.getText().toString(), passField.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("test", "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent contactListIntent = new Intent(MainActivity.this, ContactsActivity.class);
+                                    startActivity(contactListIntent);
+                                    finish();
+                                    //updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("test", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+
+                            }
+                        });
+
+
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(this, ContactsActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        //updateUI(currentUser);
     }
 }
