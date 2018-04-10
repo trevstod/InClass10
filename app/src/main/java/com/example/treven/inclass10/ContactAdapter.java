2e8c33434a519fed1b5670e32284b4a5b83c2aba
Corivana <corinna.wolfe@gmail.com>
2018-04-09 20:00:07 -0400

    Contact Adapter & Contact View
    Changed view IDs and Contact Adapter variable names. Requires more edits.

--- app/src/main/java/com/example/treven/inclass10/ContactAdapter.java
+++ app/src/main/java/com/example/treven/inclass10/ContactAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +24,16 @@ public class ContactAdapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         // you provide access to all the views for a data item in a view holder
         public static class ViewHolder extends RecyclerView.ViewHolder {
             // each data item is just a string in this case
-
+            TextView C_Name, C_Phone, C_Email;
+            ImageButton editButton, deleteButton;
 
             public ViewHolder(ConstraintLayout view) {
                 super(view);
-
+                C_Name = view.findViewById(R.id.textViewContactName);
+                C_Phone = view.findViewById(R.id.textViewPhone);
+                C_Email = view.findViewById(R.id.textViewEmail);
+                editButton = view.findViewById(R.id.imageButtonEdit);
+                deleteButton = view.findViewById(R.id.imageButtonDelete);
             }
         }
 
@@ -37,7 +43,16 @@ public class ContactAdapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             database.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-
+                    if (dataSnapshot.getValue() != null) {
+                        ArrayList<Expense> expenses = new ArrayList<>();
+                        for (DataSnapshot data : dataSnapshot.getChildren()) {
+                            HashMap<String, String> expenseMap = (HashMap<String, String>) data.getValue();
+                            Contact contact = new Contact(expenseMap.get("name"), expenseMap.get("category"), expenseMap.get("amount"), expenseMap.get("date"));
+                            contacts.add(contact);
+                        }
+                        dataset = ;
+                        notifyDataSetChanged();
+                    }
                 }
 
                 @Override
@@ -57,36 +72,24 @@ public class ContactAdapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             return vh;
         }
 
-        // Replace the contents of a view (invoked by the layout manager)
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, final int position) {
-            holder.expenseName.setText(dataset.get(position).getName());
-            holder.expenseAmt.setText(dataset.get(position).getAmount());
-            holder.csl.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    dataset.remove(dataset.get(position));
-                    notifyDataSetChanged(); // Important for updating the recycler view elements once removed
-                    Toast.makeText(view.getContext(), "Expense Deleted", Toast.LENGTH_LONG).show();
-                    return true;
-                }
-            });
-            holder.csl.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent expenseDetails = new Intent(view.getContext(), DetailExpense.class);
-                    expenseDetails.putExtra("name", dataset.get(position).getName());
-                    expenseDetails.putExtra("amount", dataset.get(position).getAmount());
-                    expenseDetails.putExtra("category", dataset.get(position).getCategory());
-                    view.getContext().startActivity(expenseDetails);
-                }
-            });
-        }
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.C_Name.setText(dataset.get(position).getName());
+        holder.C_Phone.setText(dataset.get(position).getPhone());
+        holder.C_Email.setText(dataset.get(position).getEmail());
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent expenseDetails = new Intent(view.getContext(), EditContactActivity.class);
+                expenseDetails.putExtra("name", dataset.get(position).getName());
+                expenseDetails.putExtra("amount", dataset.get(position).getAmount());
+                expenseDetails.putExtra("category", dataset.get(position).getCategory());
+                view.getContext().startActivity(expenseDetails);
+            }
+        });
+    }
 
         @Override
         public int getItemCount() {
             return dataset.size();
         }
-    }
-
 }
